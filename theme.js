2f855eea761a7a816e4357a5ed664e9072a937a6
Sylwia Brant <sylwia.brant@o2.pl>
2021-02-03 13:20:38 +0100

    Exchanged logo hosting link

--- theme.js
+++ theme.js
@@ -10,9 +10,9 @@ function removeJenkinsLogo(logoDiv){
 
 function createOSJLogo(){
     let osjLogoImg = document.createElement("IMG");
-    osjLogoImg.setAttribute('src', 'https://rawcdn.githack.com/VirtusLab/jenkins-operator-assets/dd3acbf1ecdc0cffc9102b1b537c17ef98a12d90/img/osj-logo.png');
+    osjLogoImg.setAttribute('src', 'https://rawcdn.githack.com/VirtusLab/jenkins-operator-assets/6a3d49686d5032989049416eca5e634c21cdcafb/img/osj-logo.png');
     osjLogoImg.setAttribute('alt', 'Operator Service for Jenkins Icon');
-    osjLogoImg.setAttribute('height', '60px');
+    osjLogoImg.setAttribute('height', '54px');
     osjLogoImg.setAttribute('width', 'auto');
 
     let osjLogoLink = document.createElement("a");
